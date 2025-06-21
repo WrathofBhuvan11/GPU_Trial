b4b0dc935ee2fe3bc0a48cc405131da88aa098bc
Bhuvanesh_S_K <36232037+WrathofBhuvan11@users.noreply.github.com>
2025-06-21 23:51:52 +0530

    Create scheduler.sv

---
+++ scheduler.sv
@@ -0,0 +1,18 @@
+`default_nettype none
+`timescale 1ns/1ns
+
+// Scheduler module: Determines active threads in the block based on thread count
+module scheduler #(
+    parameter THREADS_PER_BLOCK = 4
+) (
+    input wire [$clog2(THREADS_PER_BLOCK):0] thread_count, // Number of active threads
+    output reg [THREADS_PER_BLOCK-1:0] active_threads // Mask of active threads
+);
+
+    // Combinational logic to set active threads
+    always_comb begin
+        for (int t = 0; t < THREADS_PER_BLOCK; t++) begin
+            active_threads[t] = (t < thread_count); // Thread is active if index < thread_count
+        end
+    end
+endmodule
