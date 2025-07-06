c504e7df636856404e1b85b7b45d9e20a28a26dc
Bhuvanesh_S_K <36232037+WrathofBhuvan11@users.noreply.github.com>
2025-07-06 15:15:50 +0530

    Update scheduler.sv

--- scheduler.sv
+++ scheduler.sv
@@ -1,12 +1,11 @@
-`default_nettype none
 `timescale 1ns/1ns
 
 // Scheduler module: Determines active threads in the block based on thread count
 module scheduler #(
     parameter THREADS_PER_BLOCK = 4
 ) (
-    input wire [$clog2(THREADS_PER_BLOCK):0] thread_count, // Number of active threads
-    output reg [THREADS_PER_BLOCK-1:0] active_threads // Mask of active threads
+    input logic [$clog2(THREADS_PER_BLOCK):0] thread_count, // Number of active threads
+    output logic [THREADS_PER_BLOCK-1:0] active_threads // Mask of active threads
 );
 
     // Combinational logic to set active threads
